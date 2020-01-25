a0d0b5215a87bb9b66f0b174c5bed834e72083ca
ilya-msoft@mail.ru <ilya-msoft@mail.ru>
2020-01-25 22:04:18 +0500

    Ещё коммент

--- WeatherResponse.cs
+++ WeatherResponse.cs
@@ -9,6 +9,7 @@ namespace testCon
     class WeatherResponse
     {
         //comment
+        //comment2
         public TemperatureInfo Main { get; set; }
         public TemperatureInfo[] Weather { get; set; }
 
